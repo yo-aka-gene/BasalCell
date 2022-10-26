98696330ba00277a17ba9a869c0b3d5758c2b5fc
yo-aka-gene <yujiokano@keio.jp>
2022-10-26 22:50:31 +0900

    :recycle: fix bug in pre/post_gen_project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,8 +2,8 @@ import os
 import shutil
 import sys
 
-use_jup = {{cookiecutter.jupyterlab_ver}} != "none"
-use_r = {{cookiecutter.jupyterlab_ver}} != "none"
+use_jup = "{{cookiecutter.jupyterlab_ver}}" != "none"
+use_r = "{{cookiecutter.jupyterlab_ver}}" != "none"
 
 if use_jup and use_r:
     os.remove("Makefile_py")
