0f51599428bc067b4d2792c3369f826ce5338232
yo-aka-gene <yujiokano@keio.jp>
2022-10-27 22:18:35 +0900

    :wrench: change requirement files conditional

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,27 +10,36 @@ use_r = "{{cookiecutter.rstudio_ver}}" != "none"
 if use_jup and use_r:
     shutil.rmtree("jup")
     shutil.rmtree("rs")
+    shutil.rmtree("utils/jup")
+    shutil.rmtree("utils/rs")
     path = "both"
 
 elif use_jup:
     shutil.rmtree("both")
     shutil.rmtree("rs")
+    shutil.rmtree("utils/both")
+    shutil.rmtree("utils/rs")
     path = "jup"
 
 else:
     shutil.rmtree("both")
     shutil.rmtree("jup")
+    shutil.rmtree("utils/both")
+    shutil.rmtree("utils/jup")
     path = "rs"
 
 for file in glob.glob(f"{path}/*"):
     shutil.move(file, ".")
+
+for file in glob.glob(f"utils/{path}/*"):
+    shutil.move(file, "utils")
     
 os.rmdir(path)
+os.rmdir(f"utils/{path}")
 subprocess.run(["sh", "init.sh"])
 
-os.remove("docker-compose.yml")
-os.remove("auth.sh")
-os.remove("init.sh")
+for file in ["docker-compose.yml", "auth.sh", "init.sh"]:
+    os.remove(file)
 
 for file in glob.glob(f"build_{path}/*"):
     shutil.move(file, ".")
