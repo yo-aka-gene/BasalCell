21a10842fe26d78bc00d236f8a619c532f01af87
yo-aka-gene <yujiokano@keio.jp>
2022-10-27 21:02:08 +0900

    :sparkles: add make terminate in init.sh

--- {{cookiecutter.project_name}}/build_jup/init.sh
+++ {{cookiecutter.project_name}}/build_jup/init.sh
@@ -4,4 +4,4 @@ nb_id=$(id -u)
 sed -i '' -e s/YOUR_ID/${nb_id}/ $1
 docker compose up -d
 make write-lib
-# make terminate
+make terminate
--- {{cookiecutter.project_name}}/build_rs/init.sh
+++ {{cookiecutter.project_name}}/build_rs/init.sh
@@ -4,5 +4,5 @@ nb_id=$(id -u)
 sed -i '' -e s/YOUR_ID/${nb_id}/ $1
 docker compose up -d
 make write-lib
-# make terminate
+make terminate
 
