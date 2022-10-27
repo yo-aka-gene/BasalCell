8aa73cae7037f81191f0d78dbaaaec780c4054cd
yo-aka-gene <yujiokano@keio.jp>
2022-10-27 15:23:47 +0900

    :recycle: remove vacand dir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,20 +10,20 @@ use_r = "{{cookiecutter.rstudio_ver}}" != "none"
 if use_jup and use_r:
     shutil.rmtree("jup")
     shutil.rmtree("rs")
-
     for file in glob.glob("both/*"):
         shutil.move(file, ".")
+    os.rmdir("both")
 
 elif use_jup:
     shutil.rmtree("both")
     shutil.rmtree("rs")
-
     for file in glob.glob("jup/*"):
         shutil.move(file, ".")
+    os.rmdir("jup")
 
 else:
     shutil.rmtree("both")
     shutil.rmtree("jup")
-    
     for file in glob.glob("rs/*"):
         shutil.move(file, ".")
+    os.rmdir("rs")
