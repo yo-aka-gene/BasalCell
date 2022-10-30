222a0d54a6a1e621bfe53b9f1b5200ac77ac955c
yo-aka-gene <yujiokano@keio.jp>
2022-10-31 01:28:35 +0900

    :wrench: move linux pkg installation to linux_deps.sh

--- {{cookiecutter.project_name}}/both/init.sh
+++ {{cookiecutter.project_name}}/both/init.sh
@@ -3,10 +3,7 @@
 sh auth.sh docker-compose.yml
 docker compose up -d
 
-docker exec {{cookiecutter.project_name}}-rstudio-1 apt-get update
-docker exec {{cookiecutter.project_name}}-rstudio-1 apt-get install -y \
-libcurl4-openssl-dev libssl-dev libjq-dev libprotobuf-dev protobuf-compiler \
-make libgeos-dev libglpk40 libudunits2-dev libgdal-dev gdal-bin libproj-dev libv8-dev
+sh linux_deps.sh
 
 make write-lib
 sh jupyter.sh stop
