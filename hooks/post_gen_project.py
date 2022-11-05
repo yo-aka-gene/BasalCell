dcd2196fd07394a4985c681c9696380770736f15
yo-aka-gene <yujiokano@keio.jp>
2022-11-05 17:21:34 +0900

    :recycle: add codes to format tests dir in post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,6 +5,7 @@ import subprocess
 
 use_jup = "{{cookiecutter.jupyterlab_ver}}" != "none"
 use_r = "{{cookiecutter.rstudio_ver}}" != "none"
+unit_test = "{{cookiecutter.unit_test}}" != "no"
 prj_name = "{{cookiecutter.project_name}}"
 
 if use_jup and use_r:
@@ -12,6 +13,8 @@ if use_jup and use_r:
     shutil.rmtree("rs")
     shutil.rmtree("utils/jup")
     shutil.rmtree("utils/rs")
+    shutil.rmtree("tests/jup")
+    shutil.rmtree("tests/rs")
     path = "both"
 
 elif use_jup:
@@ -19,6 +22,8 @@ elif use_jup:
     shutil.rmtree("rs")
     shutil.rmtree("utils/both")
     shutil.rmtree("utils/rs")
+    shutil.rmtree("tests/both")
+    shutil.rmtree("tests/rs")
     path = "jup"
 
 else:
@@ -26,6 +31,8 @@ else:
     shutil.rmtree("jup")
     shutil.rmtree("utils/both")
     shutil.rmtree("utils/jup")
+    shutil.rmtree("tests/both")
+    shutil.rmtree("tests/jup")
     path = "rs"
 
 for file in glob.glob(f"{path}/*"):
@@ -33,9 +40,17 @@ for file in glob.glob(f"{path}/*"):
 
 for file in glob.glob(f"utils/{path}/*"):
     shutil.move(file, "utils")
+
+for file in glob.glob(f"tests/{path}/*"):
+    shutil.move(file, "tests")
     
 os.rmdir(path)
 os.rmdir(f"utils/{path}")
+os.rmdir(f"tests/{path}")
+
+if not unit_test:
+    shutil.rmtree("tests")
+
 subprocess.run(["sh", "init.sh"])
 
 for file in ["auth.sh", "init.sh"]:
