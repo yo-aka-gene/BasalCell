c3ff72542cb1d887aa22984b97e23ab424650084
yo-aka-gene <yujiokano@keio.jp>
2022-11-05 17:10:03 +0900

    :wrench: add codes for jupyter to preintall pytest and flake8 in init.sh

--- {{cookiecutter.project_name}}/both/init.sh
+++ {{cookiecutter.project_name}}/both/init.sh
@@ -5,6 +5,14 @@ docker compose up -d
 
 sh linux_deps.sh
 
+{% if cookiecutter.unit_test == "yes" %}
+docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install pytest
+{% endif %}
+
+{% if cookiecutter.lint == "yes" %}
+docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install flake8
+{% endif %}
+
 make write-lib
 sh jupyter.sh stop
 sh rstudio.sh stop
--- {{cookiecutter.project_name}}/init.sh
+++ {{cookiecutter.project_name}}/init.sh
@@ -2,6 +2,19 @@
 
 sh auth.sh docker-compose.yml
 docker compose up -d
+
+{% set use_jupyter = cookiecutter.jupyterlab_ver != 'none' -%}
+{% set unit_test = cookiecutter.unit_test != 'no' -%}
+{% set lint = cookiecutter.lint != 'no' -%}
+
+{% if use_jupyter and unit_test %}
+docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install pytest
+{% endif %}
+
+{% if use_jupyter and lint %}
+docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install flake8
+{% endif %}
+
 make write-lib
 
 if [[ -e jupyter.sh ]]; then
