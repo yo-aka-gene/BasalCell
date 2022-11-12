9565f62adb1f63a3fdd42384f653d01009bff439
yo-aka-gene <yujiokano@keio.jp>
2022-11-12 22:59:27 +0900

    :recycle::construction: restore unit_test & lint

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,27 +5,45 @@ import subprocess
 
 use_jup = "{{cookiecutter.jupyterlab_ver}}" != "none"
 use_r = "{{cookiecutter.rstudio_ver}}" != "none"
+unit_test = "{{cookiecutter.unit_test}}" != "no"
+lint = "{{cookiecutter.lint}}" != "no"
 prj_name = "{{cookiecutter.project_name}}"
 
 if use_jup and use_r:
     shutil.rmtree("utils/jup")
     shutil.rmtree("utils/rs")
+    shutil.rmtree("tests/jup")
+    shutil.rmtree("tests/rs")
     path = "both"
 
 elif use_jup:
     shutil.rmtree("utils/both")
     shutil.rmtree("utils/rs")
+    shutil.rmtree("tests/both")
+    shutil.rmtree("tests/rs")
     path = "jup"
 
 else:
     shutil.rmtree("utils/both")
     shutil.rmtree("utils/jup")
+    shutil.rmtree("tests/both")
+    shutil.rmtree("tests/jup")
     path = "rs"
 
 for file in glob.glob(f"utils/{path}/*"):
     shutil.move(file, "utils")
 
+for file in glob.glob(f"tests/{path}/*"):
+    shutil.move(file, "tests")
+
 os.rmdir(f"utils/{path}")
+os.rmdir(f"tests/{path}")
+
+if not unit_test:
+    shutil.rmtree("tests")
+
+if use_jup and not lint:
+    os.remove("utils/flake8")
 
 subprocess.run(["sh", "init.sh"])
 
--- {{cookiecutter.project_name}}/init.sh
+++ {{cookiecutter.project_name}}/init.sh
@@ -4,8 +4,18 @@ sh auth.sh docker-compose.yml
 docker compose up -d
 
 {% set use_jupyter = cookiecutter.jupyterlab_ver != 'none' -%}
+{% set unit_test = cookiecutter.unit_test != 'no' -%}
+{% set lint = cookiecutter.lint != 'no' -%}
 
 sh linux_deps.sh
+
+{% if use_jupyter and unit_test %}
+docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install pytest
+{% endif %}
+
+{% if use_jupyter and lint %}
+docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install flake8
+{% endif %}
 
 make write-lib
 sh jupyter.sh stop
