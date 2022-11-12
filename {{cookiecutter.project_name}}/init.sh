199f1e33cb705ca06124968ba961ed720d37b9fe
yo-aka-gene <yujiokano@keio.jp>
2022-11-12 21:12:47 +0900

    :recycle: merge init.sh

--- {{cookiecutter.project_name}}/init.sh
+++ {{cookiecutter.project_name}}/init.sh
@@ -4,8 +4,13 @@ sh auth.sh docker-compose.yml
 docker compose up -d
 
 {% set use_jupyter = cookiecutter.jupyterlab_ver != 'none' -%}
+{% set use_rstudio = cookiecutter.rstudio_ver != 'none' -%}
 {% set unit_test = cookiecutter.unit_test != 'no' -%}
 {% set lint = cookiecutter.lint != 'no' -%}
+
+{% if use_jupyter and use_rstudio %}
+sh linux_deps.sh
+{% endif %}
 
 {% if use_jupyter and unit_test %}
 docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install pytest
@@ -16,14 +21,8 @@ docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install flake8
 {% endif %}
 
 make write-lib
-
-if [[ -e jupyter.sh ]]; then
-    sh jupyter.sh stop
-fi
-
-if [[ -e rstudio.sh ]]; then
-    sh rstudio.sh stop
-fi
+sh jupyter.sh stop
+sh rstudio.sh stop
 
 nb_id=$(id -u)
 sed -i '' -e s/${nb_id}/YOUR_ID/ docker-compose.yml
