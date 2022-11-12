5993172ad5821b32b653a2331acc9539ead923f3
yo-aka-gene <yujiokano@keio.jp>
2022-11-12 22:40:54 +0900

    :wastebasket: remove unit_test  and lint (to merge drastic system changes first)

--- {{cookiecutter.project_name}}/init.sh
+++ {{cookiecutter.project_name}}/init.sh
@@ -4,22 +4,14 @@ sh auth.sh docker-compose.yml
 docker compose up -d
 
 {% set use_jupyter = cookiecutter.jupyterlab_ver != 'none' -%}
-{% set unit_test = cookiecutter.unit_test != 'no' -%}
-{% set lint = cookiecutter.lint != 'no' -%}
 
 sh linux_deps.sh
-
-{% if use_jupyter and unit_test %}
-docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install pytest
-{% endif %}
-
-{% if use_jupyter and lint %}
-docker exec {{cookiecutter.project_name}}-jupyterlab-1 pip install flake8
-{% endif %}
 
 make write-lib
 sh jupyter.sh stop
 sh rstudio.sh stop
 
+{% if use_jupyter %}
 nb_id=$(id -u)
 sed -i '' -e s/${nb_id}/YOUR_ID/ docker-compose.yml
+{% endif %}
