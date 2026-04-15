99edc831ce6c003bcfafc87710678530acf9d40c
yo-aka-gene <yujiokano@keio.jp>
2026-04-15 20:21:54 +0900

    :bug::pencil2: fix typo in cookiecutter.create_package

--- {{cookiecutter.__Project_Slug}}/tests/testthat.R
+++ {{cookiecutter.__Project_Slug}}/tests/testthat.R
@@ -1,7 +1,7 @@
 # tests/testthat.R
 library(testthat)
 
-{%- if cookiecutter.create_packate == "true" %}
+{%- if cookiecutter.create_package == "true" %}
 test_check("{{ cookiecutter.__project_slug }}")
 {%- else %}
 test_dir("tests/testthat")
