e565417c8310c9997209b60c02e5c571130f3d03
yo-aka-gene <yujiokano@keio.jp>
2026-04-15 19:22:32 +0900

    :sparkles: add test for BasalCell

--- tests/test_template.py
+++ tests/test_template.py
@@ -3,6 +3,7 @@ Test for BasalCell template generation
 """
 
 import pytest
+import subprocess
 
 
 @pytest.fixture
@@ -28,73 +29,122 @@ def essential_files():
 
 
 @pytest.fixture
-def essential_symbolic_links():
+def symbolic_links():
     return [
         "docs/jupyternb/data",
         "docs/jupyternb/tools",
     ]
 
+@pytest.fixture
+def rlang_files():
+    return [
+        "setup_r_env.sh",
+        "renv.lock",
+        ".Rprofile",
+        "renv/activate.R",
+        "renv/settings.json"
+    ]
 
-def test_correct_exit_code(cookies):
-    result = cookies.bake(extra_context={"project_name": "Test Project"})
+
+def uninstall_kernel(name, cwd):
+    uninstall_cmd = ["poetry", "run", "jupyter", "kernelspec", "uninstall", "-y", name]
+    subprocess.run(uninstall_cmd, cwd=cwd, capture_output=True, text=True)
+
+
+def minimal_tests(result, fixture_essential_files, fixture_symbolic_links):
+    print("===== #1. Checking exit code =====")
     assert result.exit_code == 0, \
-        f"Invalid exit_code; expected 0, got {result.exit_code}"
+        f"FAILED in #1! Invalid exit_code; expected 0, got {result.exit_code}"
 
-
-def test_correct_exception(cookies):
-    result = cookies.bake(extra_context={"project_name": "Test Project"})
+    print("===== #2. Checking exception status =====")
     assert result.exception is None, \
-        f"Invalid exception; expected None, got {result.exception}"
+        f"FAILED in #2! Invalid exception; expected None, got {result.exception}"
 
+    print("===== #3. Checking project path name =====")
+    assert result.project_path.name == "Test_Project_CI_CD", \
+        f"FAILED in #3! Invalid path; expected `Test_Project_CI_CD`, got {result.project_path.name}"
 
-def test_correct_path_name(cookies):
-    result = cookies.bake(extra_context={"project_name": "Test Project-7"})
-    assert result.project_path.name == "test_project_7", \
-        f"Invalid path; expected `test_project_7`, got {result.project_path.name}"
-
-
-def test_is_directory(cookies):
-    result = cookies.bake(extra_context={"project_name": "Test Project-7"})
+    print("===== #4. Checking project path is a directory =====")
     assert result.project_path.is_dir(), \
-        f"{result.project_path.name} is not a directory"
+        f"FAILED in #4! {result.project_path.name} is not a directory"
 
-
-def test_correct_exception(cookies):
-    result = cookies.bake(extra_context={"project_name": "Test Project"})
-    for file in essential_files():
-        assert (result.project_path / file).exists(), \
-            f"{file} is not found in {result.project_path.name}"
-        
-
-def test_essential_files_exist(cookies, essential_files):
-    result = cookies.bake(extra_context={"project_name": "Test Project"})
-    for file in essential_files:
+    print("===== #5. Checking essential files =====")
+    for i, file in enumerate(fixture_essential_files):
         file_path = result.project_path / file
         assert file_path.exists(), \
-            f"{file} is not found in {result.project_path.name}"
+            f"FAILED in #5-{i + 1}! {file} is not found in {result.project_path.name}"
 
-
-def test_symbolic_link_exists(cookies, essential_symbolic_links):
-    result = cookies.bake(extra_context={"project_name": "Test Project"})
-    for link in essential_symbolic_links:
-        link_path = result.project_path / link
-        assert link_path.is_symlink(), \
-            f"{link} is not a symbolic link"
-
-
-def test_valid_symbolic_link(cookies, essential_symbolic_links):
-    result = cookies.bake(extra_context={"project_name": "Test Project"})
-    for link in essential_symbolic_links:
-        link_path = result.project_path / link
-        assert link_path.exists(), \
-            f"Symbolic link {link} is broken (target does not exist)"
-
-
-def test_synbolic_link_for_correct_target(cookies, essential_symbolic_links):
-    result = cookies.bake(extra_context={"project_name": "Test Project"})
-    for link in essential_symbolic_links:
+    print("===== #6–8. Checking symbolic links =====")
+    for i, link in enumerate(fixture_symbolic_links):
         link_path = result.project_path / link
         expected_target_name = link.split("/")[-1]
         resolved_path = link_path.resolve()
+        # #6. If it's a symbolic link?
+        assert link_path.is_symlink(), \
+            f"FAILED in #6-{i + 1}! {link} is not a symbolic link"
+        # #7. if it's not broken
+        assert link_path.exists(), \
+            f"FAILED in #7-{i + 1}! Symbolic link {link} is broken (target does not exist)"
+        # #8. If it's referring to the correct path
         assert resolved_path.name == expected_target_name, \
-            f"{link} points to wrong target: {resolved_path.name}"
+            f"FAILED in #8-{i + 1}! {link} points to wrong target: {resolved_path.name}"
+
+    print("===== #9. Checking Jupyter Kernel =====")
+    expected_kernel_name = f"{result.project_path.name.lower()}_py"
+    try:
+        check_cmd = ["poetry", "run", "jupyter", "kernelspec", "list"]
+        res = subprocess.run(check_cmd, cwd=result.project_path, capture_output=True, text=True)
+        assert expected_kernel_name in res.stdout.lower(), \
+            f"FAILED in #9! Kernel '{expected_kernel_name}' not found.\nAvailable kernels:\n{res.stdout}"
+    finally:
+        uninstall_kernel(expected_kernel_name, result.project_path)
+
+
+def test_correct_template(cookies, essential_files, symbolic_links):
+    result = cookies.bake(extra_context={"project_name": "Test Project-CI/CD"})
+    minimal_tests(result, essential_files, symbolic_links)
+
+
+def test_correct_template_for_package_mode(cookies, essential_files, symbolic_links):
+    result = cookies.bake(
+        extra_context={
+            "project_name": "Test Project-CI/CD",
+            "create_package": "true"
+        }
+    )
+
+    minimal_tests(result, essential_files, symbolic_links)
+
+    print("===== #10. Checking Package files =====")
+    project_slug = result.project_path.name.lower()
+    file = f"src/{project_slug}/__init__.py"
+    file_path = result.project_path / file
+    assert file_path.exists(), \
+        f"FAILED in #10! {file} is not found in {result.project_path.name}"
+
+
+def test_correct_template_with_rlang(cookies, essential_files, symbolic_links, rlang_files):
+    result = cookies.bake(
+        extra_context={
+            "project_name": "Test Project-CI/CD",
+            "r_ver": "4.3"
+        }
+    )
+
+    minimal_tests(result, essential_files, symbolic_links)
+
+    print("===== #10. Checking R files =====")
+    for i, file in enumerate(rlang_files):
+        file_path = result.project_path / file
+        assert file_path.exists(), \
+            f"FAILED in #10-{i + 1}! {file} is not found in {result.project_path.name}"
+
+    print("===== #11. Checking R Kernel =====")
+    expected_kernel_name = f"{result.project_path.name.lower()}_r"
+    try:
+        check_cmd = ["poetry", "run", "jupyter", "kernelspec", "list"]
+        res = subprocess.run(check_cmd, cwd=result.project_path, capture_output=True, text=True)
+        assert expected_kernel_name in res.stdout.lower(), \
+            f"FAILED in #11! Kernel '{expected_kernel_name}' not found.\nAvailable kernels:\n{res.stdout}"
+    finally:
+        uninstall_kernel(expected_kernel_name, result.project_path)
