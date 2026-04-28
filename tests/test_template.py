03d62022c1e5ccefd73d0e48195ae20d1fcab3af
yo-aka-gene <yujiokano@keio.jp>
2026-04-28 11:38:22 +0900

    :bug: fix bug in replacing project_slug placeholder; making project slug lower case

--- tests/test_template.py
+++ tests/test_template.py
@@ -8,6 +8,11 @@ import pytest
 
 
 @pytest.fixture
+def prj_slug():
+    return "REPLACETHIS"
+
+
+@pytest.fixture
 def essential_files():
     return [
         ".gitignore",
@@ -20,13 +25,13 @@ def essential_files():
         "README.md",
         ".github/workflows/test.yml",
         ".github/pull_request_template.md",
+        "REPLACETHIS_tools/__init__.py",
         "data/.gitkeep",
         "docs/_static/default_logo.png",
         "docs/jupyternb/output/.gitkeep",
         "docs/conf.py",
         "docs/index.md",
         "tests/__init__.py",
-        "tools/__init__.py",
     ]
 
 
@@ -34,23 +39,25 @@ def essential_files():
 def symbolic_links():
     return [
         "docs/jupyternb/data",
-        "docs/jupyternb/tools",
+        "docs/jupyternb/REPLACETHIS_tools",
     ]
 
 
 @pytest.fixture
 def rlang_files():
     return [
-        "DESCRIPTION",
+        ".lintr",
         "setup_r_env.sh",
         "renv.lock",
         ".Rprofile",
-        "_pkgdown.yml",
-        "R/.gitkeep",
+        "REPLACETHIS_rtools/DESCRIPTION",
+        "REPLACETHIS_rtools/_pkgdown.yml",
+        "REPLACETHIS_rtools/R/.gitkeep",
+        "REPLACETHIS_rtools/tests/testthat.R",
+        "REPLACETHIS_rtools/tests/testthat/.gitkeep",
+        "REPLACETHIS_rtools/vignettes/example.Rmd",
         "renv/activate.R",
         "renv/settings.json",
-        "tests/testthat.R",
-        "tests/testthat/.gitkeep",
     ]
 
 
@@ -58,8 +65,8 @@ def rlang_files():
 def rlang_symbolic_links():
     return [
         "docs/jupyternb/data",
-        "docs/jupyternb/tools",
-        "tools/R",
+        "docs/jupyternb/REPLACETHIS_tools",
+        "docs/jupyternb/REPLACETHIS_rtools",
     ]
 
 
@@ -68,11 +75,13 @@ def uninstall_kernel(name, cwd):
     subprocess.run(uninstall_cmd, cwd=cwd, capture_output=True, text=True)
 
 
-def minimal_tests(result, fixture_essential_files, fixture_symbolic_links):
+def minimal_tests(result, fixture_essential_files, fixture_symbolic_links, slug):
     print("===== #1. Checking exit code =====")
-    assert (
-        result.exit_code == 0
-    ), f"FAILED in #1! Invalid exit_code; expected 0, got {result.exit_code}"
+    if result.exit_code != 0:
+        raise result.exception
+    # assert (
+    #     result.exit_code == 0
+    # ), f"FAILED in #1! Invalid exit_code; expected 0, got {result.exit_code}"
 
     print("===== #2. Checking exception status =====")
     assert (
@@ -90,6 +99,7 @@ def minimal_tests(result, fixture_essential_files, fixture_symbolic_links):
 
     print("===== #5. Checking essential files =====")
     for i, file in enumerate(fixture_essential_files):
+        file = (file).replace(slug, path.lower())
         file_path = result.project_path / file
         assert (
             file_path.exists()
@@ -97,6 +107,7 @@ def minimal_tests(result, fixture_essential_files, fixture_symbolic_links):
 
     print("===== #6–8. Checking symbolic links =====")
     for i, link in enumerate(fixture_symbolic_links):
+        link = (link).replace(slug, path.lower())
         link_path = result.project_path / link
         expected_target_name = link.split("/")[-1]
         resolved_path = link_path.resolve()
@@ -127,12 +138,14 @@ def minimal_tests(result, fixture_essential_files, fixture_symbolic_links):
         uninstall_kernel(kernel_name, result.project_path)
 
 
-def test_correct_template(cookies, essential_files, symbolic_links):
+def test_correct_template(cookies, essential_files, symbolic_links, prj_slug):
     result = cookies.bake(extra_context={"project_name": "Test Project-CI/CD"})
-    minimal_tests(result, essential_files, symbolic_links)
+    minimal_tests(result, essential_files, symbolic_links, prj_slug)
 
 
-def test_correct_template_for_package_mode(cookies, essential_files, symbolic_links):
+def test_correct_template_for_package_mode(
+    cookies, essential_files, symbolic_links, prj_slug
+):
     result = cookies.bake(
         extra_context={
             "project_name": "Test Project-CI/CD",
@@ -143,7 +156,7 @@ def test_correct_template_for_package_mode(cookies, essential_files, symbolic_li
         }
     )
 
-    minimal_tests(result, essential_files, symbolic_links)
+    minimal_tests(result, essential_files, symbolic_links, prj_slug)
 
     print("===== #10. Checking Package files =====")
     path = result.project_path.name
@@ -154,7 +167,7 @@ def test_correct_template_for_package_mode(cookies, essential_files, symbolic_li
 
 
 def test_correct_template_with_rlang(
-    cookies, essential_files, rlang_symbolic_links, rlang_files
+    cookies, essential_files, rlang_symbolic_links, rlang_files, prj_slug
 ):
     result = cookies.bake(
         extra_context={
@@ -164,11 +177,12 @@ def test_correct_template_with_rlang(
         }
     )
 
-    minimal_tests(result, essential_files, rlang_symbolic_links)
+    minimal_tests(result, essential_files, rlang_symbolic_links, prj_slug)
 
     print("===== #10. Checking R files =====")
     path = result.project_path.name
     for i, file in enumerate(rlang_files):
+        file = (file).replace(prj_slug, path.lower())
         file_path = result.project_path / file
         assert (
             file_path.exists()
