22244f39731fb092dcef2f44623aa3287f3df62c
yo-aka-gene <yujiokano@keio.jp>
2026-04-29 17:50:11 +0900

    :recycle: add script to remove initial r dependencies from environment.yml

--- {{cookiecutter.__Project_Slug}}/setup_r_env.sh
+++ {{cookiecutter.__Project_Slug}}/setup_r_env.sh
@@ -6,74 +6,39 @@ MAMBA_ENV="mamba_{{cookiecutter.__project_slug}}"
 DIR_NAME=$(basename "$PWD")
 PROJECT_NAME="${DIR_NAME}_R"
 
-echo "Building R env for ${PROJECT_NAME} (in ${MAMBA_ENV})"
+echo "Building R env for ${PROJECT_NAME}..."
 echo "=================================================="
 
 export R_LIBS_USER=""
+export R_LIBS_SITE=""
 
-if [ -f "renv.lock" ]; then
-    echo "--> Restoring R env according to renv.lock ..."
-    Rscript -e "
+Rscript --vanilla -e "
+    conda_lib <- file.path(Sys.getenv('CONDA_PREFIX'), 'lib', 'R', 'library')
+    .libPaths(conda_lib)
+
     sysname <- Sys.info()[['sysname']]
-    conda_prefix <- Sys.getenv('CONDA_PREFIX')
-
     if (sysname == 'Linux') {
-        Sys.setenv(LDFLAGS = paste0('-L', conda_prefix, '/lib -Wl,-rpath,', conda_prefix, '/lib'))
-        Sys.setenv(CPPFLAGS = paste0('-I', conda_prefix, '/include'))
-        Sys.setenv(PKG_CONFIG_PATH = paste0(conda_prefix, '/lib/pkgconfig'))
-        cran_url <- 'https://packagemanager.posit.co/cran/__linux__/jammy/latest'
+        options(repos = c(CRAN = 'https://packagemanager.posit.co/cran/__linux__/jammy/latest'), pkgType = 'source')
     } else {
-        cran_url <- 'https://packagemanager.posit.co/cran/latest'
+        options(repos = c(CRAN = 'https://cloud.r-project.org'), pkgType = 'source')
     }
-    options(repos = c(CRAN = cran_url), pkgType = 'source')
-    if (!requireNamespace('pak', quietly = TRUE)) install.packages('pak', repos = 'https://cloud.r-project.org', type = 'source')
-    if (!requireNamespace('renv', quietly = TRUE)) install.packages('renv', repos = 'https://cloud.r-project.org', type = 'source')
-    renv::restore(prompt = FALSE)
-    "
-else
-    echo "--> renv.lock is not found. Creating a new env"
-    Rscript -e "
-    sysname <- Sys.info()[['sysname']]
-    conda_prefix <- Sys.getenv('CONDA_PREFIX')
 
-    if (sysname == 'Linux') {
-        Sys.setenv(LDFLAGS = paste0('-L', conda_prefix, '/lib -Wl,-rpath,', conda_prefix, '/lib'))
-        Sys.setenv(CPPFLAGS = paste0('-I', conda_prefix, '/include'))
-        Sys.setenv(PKG_CONFIG_PATH = paste0(conda_prefix, '/lib/pkgconfig'))
-        cran_url <- 'https://packagemanager.posit.co/cran/__linux__/jammy/latest'
+    if (!file.exists('renv.lock')) {
+        message('--> Initializing new renv environment...')
+        renv::init(bare = TRUE, bioconductor = TRUE, restart = FALSE)
+        renv::snapshot(prompt = FALSE, type = 'all')
     } else {
-        cran_url <- 'https://packagemanager.posit.co/cran/latest'
+        message('--> Restoring from renv.lock...')
+        renv::restore(prompt = FALSE)
     }
-    options(repos = c(CRAN = cran_url), pkgType = 'source')
-    if (!requireNamespace('pak', quietly = TRUE)) install.packages('pak', repos = 'https://cloud.r-project.org', type = 'source')
-    if (!requireNamespace('renv', quietly = TRUE)) install.packages('renv', repos = 'https://cloud.r-project.org', type = 'source')
-    renv::init(bare = TRUE, bioconductor = TRUE)
-    "
-    Rscript -e "
-    options(renv.config.pak.enabled = TRUE)
-    renv::install(c(
-        'testthat',
-        'styler',
-        'lintr',
-        'BiocManager',
-        'IRkernel',
-        'devtools', 
-        'pkgdown', 
-        'roxygen2',
-        'rmarkdown',
-        'knitr'
-    ))
-    renv::snapshot(prompt = FALSE, type = 'all')
-    "
-fi
-
-echo "--> Registering IRkernel to Jupyter..."
-poetry run Rscript -e "
-IRkernel::installspec(
-  name = '${PROJECT_NAME}',
-  displayname = 'R ${R_VERSION} (${PROJECT_NAME})',
-  user = TRUE
-)
 "
 
+echo "--> Registering IRkernel..."
+poetry run Rscript --vanilla -e ".libPaths(file.path(Sys.getenv('CONDA_PREFIX'), 'lib', 'R', 'library')); IRkernel::installspec(name='${PROJECT_NAME}', displayname='R ${R_VERSION}', user=TRUE)"
+
 echo "=== R env setup: Done! ==="
+poetry run yq -i '.dependencies |= map(select(
+    type == "!!map" or 
+    test("=") or 
+    (test("^(r-|bioconductor-)") | not)
+))' environment.yml
