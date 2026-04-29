570cb6d4a8329bb1fc9c3b6888e1c025d49eb70b
yo-aka-gene <yujiokano@keio.jp>
2026-04-29 14:43:13 +0900

    :recycle: compile source in no-Linux env

--- {{cookiecutter.__Project_Slug}}/setup_r_env.sh
+++ {{cookiecutter.__Project_Slug}}/setup_r_env.sh
@@ -11,15 +11,34 @@ echo "=================================================="
 if [ -f "renv.lock" ]; then
     echo "--> Restoring R env according to renv.lock ..."
     Rscript -e "
-    if (!requireNamespace('pak', quietly = TRUE)) install.packages('pak', repos = 'https://cloud.r-project.org')
-    if (!requireNamespace('renv', quietly = TRUE)) install.packages('renv', repos = 'https://cloud.r-project.org')
+    sysname <- Sys.info()[['sysname']]
+    conda_prefix <- Sys.getenv('CONDA_PREFIX')
+
+    if (sysname == 'Linux') {
+        Sys.setenv(LDFLAGS = paste0('-L', conda_prefix, '/lib -Wl,-rpath,', conda_prefix, '/lib'))
+        Sys.setenv(CPPFLAGS = paste0('-I', conda_prefix, '/include'))
+        Sys.setenv(PKG_CONFIG_PATH = paste0(conda_prefix, '/lib/pkgconfig'))
+        options(repos = c(CRAN = 'https://packagemanager.posit.co/cran/__linux__/jammy/latest'))
+        options(pkgType = 'binary')
+    } else {
+        options(repos = c(CRAN = 'https://packagemanager.posit.co/cran/latest'))
+        options(pkgType = 'source')
+    }
+
+    if (!requireNamespace('pak', quietly = TRUE)) install.packages('pak', repos = 'https://cloud.r-project.org', type = getOption('pkgType'))
+    if (!requireNamespace('renv', quietly = TRUE)) install.packages('renv', repos = 'https://cloud.r-project.org', type = getOption('pkgType'))
     renv::restore(prompt = FALSE)
     "
 else
     echo "--> renv.lock is not found. Creating a new env"
     Rscript -e "
     sysname <- Sys.info()[['sysname']]
+    conda_prefix <- Sys.getenv('CONDA_PREFIX')
+
     if (sysname == 'Linux') {
+        Sys.setenv(LDFLAGS = paste0('-L', conda_prefix, '/lib -Wl,-rpath,', conda_prefix, '/lib'))
+        Sys.setenv(CPPFLAGS = paste0('-I', conda_prefix, '/include'))
+        Sys.setenv(PKG_CONFIG_PATH = paste0(conda_prefix, '/lib/pkgconfig'))
         cran_url <- 'https://packagemanager.posit.co/cran/__linux__/jammy/latest'
         pkg_type <- 'binary'
     } else {
@@ -28,8 +47,8 @@ else
     }
     options(repos = c(CRAN = cran_url))
     options(pkgType = pkg_type)
-    if (!requireNamespace('pak', quietly = TRUE)) install.packages('pak', repos = 'https://cloud.r-project.org')
-    if (!requireNamespace('renv', quietly = TRUE)) install.packages('renv', repos = 'https://cloud.r-project.org')
+    if (!requireNamespace('pak', quietly = TRUE)) install.packages('pak', repos = 'https://cloud.r-project.org', type = getOption('pkgType'))
+    if (!requireNamespace('renv', quietly = TRUE)) install.packages('renv', repos = 'https://cloud.r-project.org', type = getOption('pkgType'))
     renv::init(bare = TRUE, bioconductor = TRUE)
     "
     Rscript -e "
