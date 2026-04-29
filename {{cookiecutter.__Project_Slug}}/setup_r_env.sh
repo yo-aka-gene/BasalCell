9a5e1975cb5844e30c336a63c590364ea062a57e
yo-aka-gene <yujiokano@keio.jp>
2026-04-29 23:23:55 +0900

    :recycle::bug: fix r-base r-renv r-irkernel versions in environment.yml

--- {{cookiecutter.__Project_Slug}}/setup_r_env.sh
+++ {{cookiecutter.__Project_Slug}}/setup_r_env.sh
@@ -33,12 +33,30 @@ Rscript --vanilla -e "
     }
 "
 
-echo "--> Registering IRkernel..."
-poetry run Rscript --vanilla -e ".libPaths(file.path(Sys.getenv('CONDA_PREFIX'), 'lib', 'R', 'library')); IRkernel::installspec(name='${PROJECT_NAME}', displayname='R ${R_VERSION} (${PROJECT_NAME})', user=TRUE)"
+RBASE_VER=$(mamba list -n "$MAMBA_ENV" "^r-base$" | awk '/r-base/ {print $2}')
+RENV_VER=$(mamba list -n "$MAMBA_ENV" "^r-renv$" | awk '/r-renv/ {print $2}')
+IRKERNEL_VER=$(mamba list -n "$MAMBA_ENV" "^r-irkernel$" | awk '/r-irkernel/ {print $2}')
 
-echo "=== R env setup: Done! ==="
-poetry run yq -y -i '.dependencies |= map(select(
+echo "--> Locking Infra: r-base=${RBASE_VER}, r-renv=${RENV_VER}, r-irkernel=${IRKERNEL_VER}"
+
+perl -pi -e "s/- \"?r-base\"?$/- r-base=$RBASE_VER/; \
+             s/- \"?r-renv\"?$/- r-renv=$RENV_VER/; \
+             s/- \"?r-irkernel\"?$/- r-irkernel=$IRKERNEL_VER/" environment.yml
+
+echo "--> Registering IRkernel..."
+Rscript --vanilla -e "
+    .libPaths(file.path(Sys.getenv('CONDA_PREFIX'), 'lib', 'R', 'library'))
+    IRkernel::installspec(
+        name='${PROJECT_NAME}_r', 
+        displayname='R ${RBASE_VER} (${PROJECT_NAME})', 
+        user=TRUE
+    )
+"
+
+yq -y -i '.dependencies |= map(select(
     type == "!!map" or 
     test("=") or 
     (test("^(r-|bioconductor-)") | not)
 ))' environment.yml
+
+echo "=== R env setup: Done! ==="
