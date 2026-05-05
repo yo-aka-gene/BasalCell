45b2fb79ef1effef50e134b16073ffaee0ec7f6b
yo-aka-gene <yujiokano@keio.jp>
2026-05-05 18:03:20 +0800

    :sparkles: add make dump report

--- {{cookiecutter.__Project_Slug}}/.basalcell/basalcell_system/__init__.py
+++ {{cookiecutter.__Project_Slug}}/.basalcell/basalcell_system/__init__.py
@@ -1,17 +1,31 @@
+from ._export import export, export_all_dependencies, export_essentials, report
 {%- if cookiecutter.r_ver != "none" %}
 from ._query import print_renv_targets, query, query_essentials
 from ._read import read_database, read_lookup
 
 __all__ = [
-    "read_database",
-    "read_lookup",
+    "export",
+    "export_all_dependencies",
+    "export_essentials",
+    "report",
+    "print_renv_targets",
     "query",
     "query_essentials",
-    "print_renv_targets",
+    "read_database",
+    "read_lookup",
 ]
 {%- else %}
 from ._query import query, query_essentials
 from ._read import read_database, read_lookup
 
-__all__ = ["read_database", "read_lookup", "query", "query_essentials"]
+__all__ = [
+    "export",
+    "export_all_dependencies",
+    "export_essentials",
+    "report",
+    "query",
+    "query_essentials",
+    "read_database",
+    "read_lookup",
+]
 {%- endif %}
